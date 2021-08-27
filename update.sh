c30bd4a1a3cabc4abec28f017a1ef778ae1d0a00
alois <=>
2021-08-27 22:15:17 +0200

    fixed double ';' on var declaration in c code

--- update.sh
+++ update.sh
@@ -19,6 +19,7 @@ fi
 echo "╭───────────────────╮"
 echo "│Updating repository│ Note: this will fail if you"
 echo "╰───────────────────╯ made any modification to it."
+echo ""
 git pull origin main
 # Installing the new version
 sudo chmod +x ./install.sh
