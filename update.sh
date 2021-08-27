6e72efb05237f6cb13c494793fbdc22ed2d419bd
alois <=>
2021-08-27 22:01:26 +0200

    added update check to update script

--- update.sh
+++ update.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-# Fetching the repo
+# Check if any update is available
+git fetch
+HEADHASH=$(git rev-parse HEAD)
+UPSTREAMHASH=$(git rev-parse master@{upstream})
+if [ "$HEADHASH" == "$UPSTREAMHASH" ]
+then
+    echo -e "\033[1;32m╭────────────────────────╮"
+    echo              "│Everything is up to date│"
+    echo              "╰────────────────────────╯\033[0m"
+    exit 0
+fi
+
+# Updating the repo
 echo "╭───────────────────╮"
 echo "│Updating repository│ Note: this will fail if you"
 echo "╰───────────────────╯ made any modification to it."
