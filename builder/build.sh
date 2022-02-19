ff3a65df0be44dd1a27c6b0a9c6dbb0c461b3edc
alois <=>
2022-02-19 16:19:29 +0100

    new build

--- builder/build.sh
+++ builder/build.sh
@@ -3,13 +3,16 @@
 cd ../src
 
 # linux
-echo "---> LINUX BUILD"
+echo -e "\n---> LINUX BUILD\n"
 bah ./main.bah -o ../bin/linux_amd64
+bah ./main.bah -o ../installer/linux.c -c
 
 # windows
-echo "---> WINDOWS BUILD"
+echo -e "\n---> WINDOWS BUILD\n"
 bah ./main.bah -o ../bin/windows.exe -CC x86_64-w64-mingw32-gcc -target windows
+bah ./main.bah -o ../installer/windows.c -CC x86_64-w64-mingw32-gcc -target windows -c
 
 # dariwn (not working at the moment)
-# echo "---> DARWIN BUILD"
-# bah ./main_darwin.bah -o ../bin/darwin_amd64 -CC ../builder/osxcross/target/bin/x86_64h-apple-darwin14-clang -target darwin
+echo "\n---> DARWIN BUILD\n"
+# bah ./main.bah -o ../bin/darwin_amd64 -CC ../builder/osxcross/target/bin/x86_64h-apple-darwin14-clang -target darwin
+bah ./main.bah -o ../installer/darwin.c -CC ../builder/osxcross/target/bin/x86_64h-apple-darwin14-clang -target darwin -c
