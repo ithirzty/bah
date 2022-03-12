c7949aadb853175f08cb404568df5aa63f18d2b4
alois <=>
2022-03-12 16:41:15 +0100

    optimizations for faster compile time

--- builder/build.sh
+++ builder/build.sh
@@ -4,13 +4,13 @@ cd ../src
 
 # linux
 echo -e "\n---> LINUX BUILD\n"
-bah ./main.bah -o ../bin/linux_amd64
+bah ./main.bah -o ../bin/linux_amd64 -verboseCC
 bah ./main.bah -o ../installer/linux.c -c
 sudo cp ../bin/linux_amd64 /opt/bah/bah
 
 # windows
 echo -e "\n---> WINDOWS BUILD\n"
-bah ./main.bah -o ../bin/windows.exe -CC x86_64-w64-mingw32-gcc -target windows
+bah ./main.bah -o ../bin/windows.exe -CC x86_64-w64-mingw32-gcc -target windows -verboseCC
 bah ./main.bah -o ../installer/windows.c -CC x86_64-w64-mingw32-gcc -target windows -c
 
 # dariwn (not working at the moment)
