2571f88b956ee982a6665e812ce9471b5c8d8993
alois <=>
2022-11-22 20:21:23 +0100

    Bah v1.4 [compiler] - added -noImports (enabled by -c) for redirecting #import to #include - std libs are now imported (faster compiler compilation time, should not change compiler performance) - added token reference to var search for error handling (no more "cannot use '*'" error without location informations) - refactored symbol lookup inside the compiler (a lot of code has changed, possible bugs?) -> should be faster and more reliable   -> replaced sketchy variable search by keeping reference to variable on the token -> searchVirtVarByToken()   -> replaced sketchy function search by keeping reference to func on the token -> searchVirtFuncByToken()   -> replaced sketchy array reconstruction for c emition in genArrRealloc() (var.arrayRoot and var.arrayIndex)   -> vars does not have C code as name 💀 (thank god) - array reallocation does not emit unnecessary variable anymore - forced type checking on Tok.bahRef (not a shortcut anymore) - fixed &struct.member emitting &struct->member
    [builtin.bah]
    - started adding bounds checking
    
    [debug.bah]
    - fixed symbol printing

--- builder/build.sh
+++ builder/build.sh
@@ -10,12 +10,12 @@ sudo cp ../bin/linux_amd64 /opt/bah/bah
 
 # windows
 echo -e "\n---> WINDOWS BUILD\n"
-bah ./main.bah -o ../bin/windows.exe -CC x86_64-w64-mingw32-gcc -target windows -verboseCC -n
+bah ./main.bah -o ../bin/windows.exe -CC x86_64-w64-mingw32-gcc -target windows -verboseCC -n -noImports
 bah ./main.bah -o ../installer/windows.c -CC x86_64-w64-mingw32-gcc -target windows -c -n
 
 # dariwn (not working at the moment)
 echo -e "\n---> DARWIN BUILD\n"
-# bah ./main.bah -o ../bin/darwin_amd64 -CC ../builder/osxcross/target/bin/x86_64h-apple-darwin14-clang -target darwin
+# bah ./main.bah -o ../bin/darwin_amd64 -CC ../builder/osxcross/target/bin/x86_64h-apple-darwin14-clang -target darwin -noImports
 bah ./main.bah -o ../installer/darwin.c -CC ../builder/osxcross/target/bin/x86_64h-apple-darwin14-clang -target darwin -c -n
 
 echo -e "\n---> adding std libs to installer"
