467991828c5faaafa3510f20a3574e790d41066f
alois <=>
2022-11-24 18:54:34 +0100

    [compiler] - optimizations: skip reading to memory/line the body of a function in 'should only declare' mode (enable parsing an imported file) - fixed __dumpSymbols() making the compiler crash
    [installer]
    - working on a fix for windows install

--- builder/build.sh
+++ builder/build.sh
@@ -10,7 +10,8 @@ sudo cp ../bin/linux_amd64 /opt/bah/bah
 
 # windows
 echo -e "\n---> WINDOWS BUILD\n"
-bah ./main.bah -o ../bin/windows.exe -CC x86_64-w64-mingw32-gcc -target windows -verboseCC -n -noImports
+# BROKEN: mingw32 cant cross compile, segfault in 
+# bah ./main.bah -o ../bin/windows.exe -CC x86_64-w64-mingw32-gcc -target windows -verboseCC -n -noImports
 bah ./main.bah -o ../installer/windows.c -CC x86_64-w64-mingw32-gcc -target windows -c -n
 
 # dariwn (not working at the moment)
--- windows_compile_install.bat
+++ windows_compile_install.bat
@@ -4,5 +4,5 @@ mkdir "C:/Bah"
 xcopy /E/H/C/I/Y "./common" "C:/Bah"
 xcopy /E/H/C/I/Y "./windows" "C:/Bah"
 cd ../
-gcc windows.c -w  -static C:/Bah/libs/gc.a -lpthread -lm
+gcc windows.c -w  -static -I "C:/Bah/libs/windows/include/" -L "C:/Bah/libs/windows/" -l:gc.a -O1-lpthread -lm
 MOVE ./a.exe C:/Windows/bah.exe
